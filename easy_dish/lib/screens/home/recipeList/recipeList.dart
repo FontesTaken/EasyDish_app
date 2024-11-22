2982723b5afbeeef96247164c3905af237e51c3e
FontesTaken <91680144+FontesTaken@users.noreply.github.com>
2024-11-22 12:57:23 +0000

    Imagem deve aparecer na lista de recipes na homepage

--- easy_dish/lib/screens/home/recipeList/recipeList.dart
+++ easy_dish/lib/screens/home/recipeList/recipeList.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart'; // Import kIsWeb
 import 'package:flutter/material.dart';
 
 import '../../../data_classes/recipe_data.dart';
@@ -18,19 +19,33 @@ Expanded recipeList() {
           child: ListTile(
             leading: ClipRRect(
               borderRadius: BorderRadius.circular(15.0),
-              child: Image.network(
-                recipe.imageUrl,
-                width: 100,
-                height: 100,
-                fit: BoxFit.cover,
-              ),
+              child: kIsWeb
+                  ? Image.network(
+                      recipe.imageUrl, // Use `imageUrl` for web
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    )
+                  : (recipe.imageUrl.startsWith('http')
+                      ? Image.network(
+                          recipe.imageUrl, // For non-web with URLs
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        )
+                      : Image.file(
+                          File(recipe.imageUrl), // For local file paths
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        )),
             ),
             title: Text(
               recipe.name,
               style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF885B0E)
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF885B0E),
               ),
             ),
             subtitle: Column(
@@ -39,9 +54,8 @@ Expanded recipeList() {
                 const SizedBox(height: 8.0), // Add spacing to the top
                 Wrap(
                   spacing: 4.0,
-                  children: recipe.tags
-                      .map((tag) => TagWidget(text: tag))
-                      .toList(),
+                  children:
+                      recipe.tags.map((tag) => TagWidget(text: tag)).toList(),
                 ),
               ],
             ),
@@ -49,8 +63,7 @@ Expanded recipeList() {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) =>
-                      RecipeDetailPage(recipe: recipe),
+                  builder: (context) => RecipeDetailPage(recipe: recipe),
                 ),
               );
             },
